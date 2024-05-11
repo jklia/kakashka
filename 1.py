1fa0678a458420a365875850f9c118590c1b6d6b
Pomidorka Yaroslav <52704200+VirtualniyPomidor@users.noreply.github.com>
2024-05-11 23:34:06 +0500

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-from time import *
-timer1 = time()
-sleep(4)
-list = []
-for i in range(1, 1000):
-    for j in range(i):
-        list.append(j**22)
-timer2 = time()
-print(timer2-timer1, (timer2-timer1)%1)
